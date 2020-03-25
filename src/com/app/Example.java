5262c25b7053b154c39a11640710376545a94481
Gulaydemir33 <gulaydemir25@gmail.com>
2020-03-25 17:48:51 -0500

    Initial commit

---
+++ src/com/app/Example.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class Example {
+    public static void main(String[] args) {
+
+        String myString="hello";
+        System.out.println(myString);
+        System.out.println(myString.toUpperCase());
+    }
+}
